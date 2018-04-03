018185d9b7f5a31d1ead8224f35e8bdcf5411598
Kim Foukaris <kimfoukaris@outlook.com>
2018-04-03 04:28:21 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,37 +9,31 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/posts/new' do
-    binding.pry
     erb :new
   end
 
   post '/posts' do
-    binding.pry
     @post = Post.create(name: params[:name], content: params[:content])
     erb :show
   end
 
   get '/posts' do
-    binding.pry
     @posts = Post.all
     erb :index
   end
 
   get '/posts/:id' do
-    binding.pry
     @post = Post.find(params[:id])
     erb :show
   end
 
   get '/posts/:id/edit' do
-    binding.pry
     @post = Post.find(params[:id])
     erb :edit
   end
 
   patch '/posts/:id' do
     @post = Post.find(params[:id])
-    binding.pry
     @post = Post.update(params[:id], name: params[:name], content: params[:content])
     erb :show
   end
