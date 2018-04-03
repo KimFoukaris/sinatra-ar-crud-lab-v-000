69a97f2aaccd29dbfd1c74c708752da849dd4996
Kim Foukaris <kimfoukaris@outlook.com>
2018-04-03 04:03:31 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,25 +9,30 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/posts/new' do
+    binding.pry
     erb :new
   end
 
   post '/posts' do
+    binding.pry
     @post = Post.create(name: params[:name], content: params[:content])
     erb :show
   end
 
   get '/posts' do
+    binding.pry
     @posts = Post.all
     erb :index
   end
 
   get '/posts/:id' do
+    binding.pry
     @post = Post.find(params[:id])
     erb :show
   end
 
   get '/posts/:id/edit' do
+    binding.pry
     @post = Post.find(params[:id])
     erb :edit
   end
@@ -41,6 +46,7 @@ class ApplicationController < Sinatra::Base
   end
 
   delete '/posts/:id/delete' do
+    binding.pry
     erb :show_delete
   end
 
